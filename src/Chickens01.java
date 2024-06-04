e4cd432f380670fa63b9fa9e0e15b706675c3695
FabioBritto <fabio.tritono@gmail.com>
2024-06-04 16:57:45 -0300

    Commit project Chickens01

---
+++ src/Chickens01.java
@@ -0,0 +1,13 @@
+public class Chickens01 {
+    public static void main(String[] args) {
+        int count=15;
+        int a, b, c;// dias da semana que as galinhas irão botar os ovos 
+        a = count++;
+        b = count + 4; c = count - 6;
+       
+        
+        System.out.println(a + ", " + b + ", " + c + ", ");
+        
+    }   
+}
+
